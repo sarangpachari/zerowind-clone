bc9dcd2e8bfe2513e2cb97ee8a0526b1eefda5cc
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-04-05 17:01:37 +0530

    technologies completed without animation

---
+++ src/components/Home/Technologies.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Fit from "./Technologies/Fit";
+import Motion from "./Technologies/Motion";
+import Power from "./Technologies/Power";
+import Brave from "./Technologies/Brave";
+
+const Technologies = () => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center gap-8 px-12">
+      <div className="flex gap-8 w-full justify-evenly items-center">
+        <Fit />
+        <Motion />
+      </div>
+      <div className="flex gap-8 w-full justify-evenly items-center">
+        <Power />
+        <Brave />
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
