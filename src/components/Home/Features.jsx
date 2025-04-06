636cc29e6d163c846b27e8a3c084c0a74938fc7c
kiran <kiranuthaman85@gmail.com>
2025-04-06 15:09:16 +0530

    added new mouse feature and made feature section responsive

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -4,14 +4,12 @@ import Zwr from './features/Zwr'
 
 function Features() {
   return (
-    <>
-     <div className="w-full flex flex-col justify-center items-center gap-8 px-12 mt-5">
-      <div className="flex gap-8 w-full justify-evenly items-center">
-      <Relife/>
-     <Zwr/>    
+    <div className="w-full flex flex-col justify-center items-center gap-8 px-6 sm:px-12 mt-5">
+      <div className="flex flex-col md:flex-row gap-8 w-full justify-evenly items-center sm:pb-5">
+        <Relife />
+        <Zwr />
       </div>
     </div>
-    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroBanner from "../components/Home/HeroBanner";
 import Technologies from "../components/Home/Technologies";
 import About from "../components/Home/About";
-
 import Contact from "../components/Home/Contact";
 import Features from "../components/Home/Features";
-
-
+import CustomCursor from "../components/CustomCursor";
 
 const Home = () => {
+  const [showCursor, setShowCursor] = useState(false);
+
+  const enableCursor = () => setShowCursor(true);
+  const disableCursor = () => setShowCursor(false);
+
   return (
     <>
+      <CustomCursor visible={showCursor} />
+
       <div className="gray-bg">
         <Navbar />
         <HeroBanner />
-        <Technologies />
+        
+        <div onMouseEnter={enableCursor} onMouseLeave={disableCursor}>
+          <Technologies />
+        </div>
         <About />
-        <Features/>
+
+        <div onMouseEnter={enableCursor} onMouseLeave={disableCursor}>
+        <Features />
+        </div>
       </div>
-      <Contact/>
-      <Footer/>
+      
+      <Contact />
+      <Footer />
     </>
   );
 };
