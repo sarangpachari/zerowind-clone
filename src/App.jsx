1fcdf8a57f83e1a73ec2c68f6d40e7699fd7f040
kiran <kiranuthaman85@gmail.com>
2025-04-07 13:50:21 +0530

    added a preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import Preloader from "./components/Preloader";
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading process
+    const timer = setTimeout(() => setLoading(false), 1800); 
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <Home />
+     {loading ? <Preloader /> :  <Home />}
+     
     </>
   );
 }
